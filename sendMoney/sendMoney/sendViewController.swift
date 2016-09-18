59bbfe133a1bc43655b14801a1748df3255c4fd0
lkcallen <lkcallen@gmail.com>
2016-09-18 06:07:12 -0700

    Added .zip and individual files via upload
    HACKMIT2016

---
+++ sendMoney/sendMoney/sendViewController.swift
@@ -0,0 +1,39 @@
+//
+//  sendViewController.swift
+//  sendMoney
+//
+//  Created by vagrant on 18/09/2016.
+//  Copyright © 2016 HackMIT. All rights reserved.
+//
+
+import UIKit
+
+class sendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //hides keyboard
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
